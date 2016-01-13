5710424a3bec4f095772dc2f8bc7d1cb00b7930f
Eric King <e.king117@gmail.com>
2016-01-13 11:31:24 -0500

    Added babel, made some es6 modifications

--- es6/palindromes.js
+++ es6/palindromes.js
@@ -1,42 +1,41 @@
-'use strict';
-
-var Palindrome = function () {
+var Palindrome = (() => {
   "use strict";
   //checks if word is a a palindrome, returns boolean
-
-  var isPalindrome = function isPalindrome(word) {
-    if (typeof word === 'number') {
+  var isPalindrome = function(word){
+    if (typeof word === 'number'){
       word = String(word);
     }
     if (word === '' || word.length === 1) return false;
-    if (typeof word === 'string') {
-      return word.replace(/\s/, '').toLowerCase() === word.split('').reverse().join('').replace(/\s/, '').toLowerCase();
+    if (typeof word === 'string'){
+      return word.replace(/\s/, '').toLowerCase() === 
+        word.split('').reverse().join('').replace(/\s/, '').toLowerCase();
     } else return false;
   };
 
   //returns the factorial of a number, returns NaN for negative and fractional numbers
-  var factorial = function () {
-    var mem = { 0: 1, 1: 1 };
-    return function (num) {
+  var factorial = (function() {
+    var mem = {0: 1, 1: 1};
+    return function(num){
       if (num in mem) {
         return mem[num];
       }
       if (num >= 0 && num % 1 === 0 && typeof num === 'number') {
         mem[num] = num * factorial(num - 1);
         return mem[num];
-      } else return NaN;
+      } else return NaN; 
     };
-  }();
+    
+  })();
 
   //returns the factorial of the number of palindromes within an array
-  var palindromeFactorial = function palindromeFactorial(arr) {
-    if (Array.isArray(arr)) {
-      return factorial(arr.filter(function (item) {
+  var palindromeFactorial = function(arr){
+    if(Array.isArray(arr)){
+      return factorial(arr.filter( (item) => {
         return isPalindrome(item);
       }).length);
     } else return NaN;
   };
 
   //returns an object, makes these methods available
-  return { isPalindrome: isPalindrome, factorial: factorial, palindromeFactorial: palindromeFactorial };
-}();
+  return {isPalindrome, factorial, palindromeFactorial};
+})();
