34c9d8608e3f72582179154b1dd89eff434ffb2c
Eric King <e.king117@gmail.com>
2016-01-12 19:51:07 -0500

    Converted to module pattern and added tests.

--- Spec.js
+++ Spec.js
@@ -1,20 +1,38 @@
 describe("Palindromes", function() {
   it("should solve factorials", function(){
-    expect(Math.factorial(5)).to.equal(120);
-    expect(Math.factorial(4)).to.equal(24);
-    expect(Math.factorial(3)).to.equal(6);
-    expect(Math.factorial(2)).to.equal(2);
-    expect(Math.factorial(1)).to.equal(1);
-    expect(Math.factorial(0)).to.equal(1);
-    expect(isNaN(Math.factorial(-1))).to.be.true;
-    expect(isNaN(Math.factorial('carrot'))).to.be.true;
-    expect(isNaN(Math.factorial({}))).to.be.true;
-    expect(isNaN(Math.factorial([]))).to.be.true;
+    expect(Palindrome.factorial(5)).to.equal(120);
+    expect(Palindrome.factorial(4)).to.equal(24);
+    expect(Palindrome.factorial(3)).to.equal(6);
+    expect(Palindrome.factorial(2)).to.equal(2);
+    expect(Palindrome.factorial(1)).to.equal(1);
+    expect(Palindrome.factorial(0)).to.equal(1);
+    expect(isNaN(Palindrome.factorial(7.95))).to.be.true;
+    expect(isNaN(Palindrome.factorial(-1))).to.be.true;
+    expect(isNaN(Palindrome.factorial('carrot'))).to.be.true;
+    expect(isNaN(Palindrome.factorial({}))).to.be.true;
+    expect(isNaN(Palindrome.factorial([]))).to.be.true;
   });
 
   it("check if words are palindromes", function() {
-    expect('racecar'.isPalindrome()).to.be.true;
-    expect('dad'.isPalindrome()).to.be.true;
-    expect('cat'.isPalindrome()).to.be.false;
+    expect(Palindrome.isPalindrome('racecar')).to.be.true;
+    expect(Palindrome.isPalindrome('dad')).to.be.true;
+    expect(Palindrome.isPalindrome('cat')).to.be.false;
+    expect(Palindrome.isPalindrome('')).to.be.false;
+    expect(Palindrome.isPalindrome([])).to.be.false;
+    expect(Palindrome.isPalindrome({})).to.be.false;
+    expect(Palindrome.isPalindrome(43)).to.be.false;
+  });
+
+  it("Returns factorial of the number of palindromes in an array", function() {
+    expect(Palindrome.palindromeFactorial(
+      ['mom', 'race car', 'dad', 'florence healthcare'])).to.equal(6);
+    expect(Palindrome.palindromeFactorial(
+      [])).to.equal(1);
+    expect(Palindrome.palindromeFactorial(
+      ['Amy must I jujitsu my ma'])).to.equal(1);
+    expect(isNaN(Palindrome.palindromeFactorial(7.95))).to.be.true;
+    expect(isNaN(Palindrome.palindromeFactorial(-1))).to.be.true;
+    expect(isNaN(Palindrome.palindromeFactorial('carrot'))).to.be.true;
+    expect(isNaN(Palindrome.palindromeFactorial({}))).to.be.true;
   });
 });
--- palindromes.js
+++ palindromes.js
@@ -1,19 +1,39 @@
 "use strict";
 
-String.prototype.isPalindrome = function(){
-  var word = this;
-  if (typeof word === 'string'){
-    return word === word.split('').reverse().join('');
-  } else return false;
-};
+var Palindrome = (function(){
+  //checks if word is a a palindrome, returns boolean
+  var isPalindrome = function(word){
+    if (word === '') return false;
+    if (typeof word === 'string'){
+      return word.replace(/\s/, '') === word.split('').reverse().join('').replace(/\s/, '');
+    } else return false;
+  };
 
-Math.factorial = function(num){
-  if (typeof num === 'number') {
-    if (num === 0) return 1;
-    if (num < 0) return NaN;
-    for (var i = num - 1; i >= 1; i--){
-      num *= i;
-    }
-    return num;
-  } else return NaN;
-};
+  //returns the factorial of a number, returns NaN for negative and fractional numbers
+  var factorial = function(num){
+    if (typeof num === 'number') {
+      if (num === 0) return 1;
+      if (num < 0 || num % 1 !== 0) return undefined;
+      for (var i = num - 1; i >= 1; i--){
+        num *= i;
+      }
+      return num;
+    } else return NaN;
+  };
+
+  //returns the factorial of the number of palindromes within an array
+  var palindromeFactorial = function(arr){
+    if(Array.isArray(arr)){
+      return factorial(arr.filter(function(item){
+        return isPalindrome(item);
+      }).length);
+    } else return NaN;
+  };
+
+  //returns an object, makes these methods available
+  return {
+    isPalindrome: isPalindrome,
+    factorial: factorial,
+    palindromeFactorial: palindromeFactorial
+  };
+})();
